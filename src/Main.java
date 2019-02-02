a59ca861824bf10cb2972ac4434443702103f7a8
HAMOUNI Sofiane Ext TGI/OLS <KGNX2971@orange.com>
2019-02-02 15:20:05 +0100

    test1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,63 @@
-
-public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("hello world !");
-
+import java.io.*;
+import java.util.*;
+public class Main
+{
+public static void main (String[]args)
+							{
+								try
+									{
+ 	
+									@SuppressWarnings("resource")
+	
+									BufferedReader b = new BufferedReader (new FileReader ("test1.txt" ));
+ 	
+									String line;
+ 	
+									Map<String, Integer> maMap = new HashMap<String, Integer>();
+ 	
+									Integer Mot = new Integer(1);
+ 	
+									while ((line = b.readLine())!= null)
+ 	
+									{
+   
+										String [] tokens = line.split("\\b" );
+   
+										int i = 0;
+   
+										while (tokens.length > i)
+  
+										{
+   	
+											String Word = tokens[i++];
+   	
+											Word = Word.toLowerCase();
+   	
+											Word = Word.replaceAll("[^a-z]","" );
+   	
+											if(Word.length()>2)
+											{
+												if (maMap.containsKey(Word)== false )
+												{
+													maMap.put(Word, Mot);
+												}
+												else
+												{
+													String nombre_key = maMap.get(Word).toString();
+													Integer MotStoke = new Integer(nombre_key);
+													int temp = Mot.intValue()+ MotStoke.intValue();
+													Integer nbMot = new Integer (temp);
+													Mot.toString();
+													maMap.remove(Word);
+													maMap.put(Word, nbMot);
+												}
+											}
+										}
+									}
+									System.out.println(maMap);
+									}
+								catch (Exception e) {
+									System.out.println(e);
+								}
+							}
 	}
-
-}
