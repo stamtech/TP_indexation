f59002a25958552c777a312d8c3142bdb82c3df3
HAMOUNI Sofiane Ext TGI/OLS <KGNX2971@orange.com>
2019-02-02 15:27:49 +0100

    test1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import java.io.*;
 import java.util.*;
 public class Main
-{
+					{
 public static void main (String[]args)
 							{
 								try
+								
+								
 									{
  	
 									@SuppressWarnings("resource")
